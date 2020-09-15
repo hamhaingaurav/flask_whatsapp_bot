ca81d06df4ce004e3a964fc5407ea82ab6d652b3
Gaurav Sharma <gaurav91297@gmail.com>
2020-09-15 18:31:50 +0530

    Index page included

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
-from flask import Flask, request, jsonify, make_response
+from flask import Flask, request, make_response
 
 from wabot import WaBot
 
 
 app = Flask(__name__)
 
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['POST','GET'])
 def home():
     if request.method == 'POST':
         bot = WaBot(request.json)
         return bot.processing()
+    elif request.method == 'GET':
+        response = make_response('<h1>Welcome to the Whatsapp Bot Build by Gaurav</h1>', 200)
+        response.mimetype = "text/html"
+        return response
 
 
 if(__name__) == '__main__':
